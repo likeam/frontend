2e7dfe773b305369c1570c73e404e58b5b188dbc
Abdul Rehman <likeam99@gmail.com>
2024-05-23 12:22:24 +0500

    Building handleAppointment function in AppointmentForm Section

--- src/components/AppointmentForm.jsx
+++ src/components/AppointmentForm.jsx
@@ -1,4 +1,6 @@
-import React, { lazy, useState } from "react";
+import axios from "axios";
+import React, { lazy, useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 const AppointmentForm = () => {
   const [firstName, setFirstName] = useState("");
@@ -32,11 +34,70 @@ const AppointmentForm = () => {
     "Oncology",
   ];
 
+  const [doctors, setDoctrs] = useState([]);
+
+  useEffect(() => {
+    const fetchDoctors = async () => {
+      const { data } = await axios.get(
+        "http://localhost:4000/api/v1/user/doctors",
+        { withCredentials: true }
+      );
+      setDoctrs(data.doctor);
+      console.log(data.doctor);
+    };
+    fetchDoctors();
+  }, []);
+
+  const handleAppointment = async (event) => {
+    event.preventDefault();
+    try {
+      const hasVisitedBool = Boolean(hasVisited);
+      const { data } = await axios.post(
+        "http://localhost:4000/api/v1/appointment",
+        {
+          firstName,
+          lastName,
+          email,
+          phone,
+          nic,
+          dob,
+          gender,
+          appointmentDate,
+          department,
+          doctorFirstName,
+          doctorLastName,
+          adress,
+          hasVisited: hasVisitedBool,
+        },
+        {
+          withCredentials: true,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      toast.success(data.message);
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+      setPhone("");
+      setNic("");
+      setDob("");
+      setGender("");
+      setAppointmentDate("");
+      setDepartment("");
+      setDoctorFirstName("");
+      setDoctorLastName("");
+      setAdress("");
+      setVisited("");
+    } catch (error) {
+      toast.error(error.respone.data.message);
+    }
+  };
+
   return (
     <>
       <div className="container form-component appointment-form">
         <h2>Appointment</h2>
-        <form>
+        <form onSubmit={handleAppointment}>
           <div>
             <input
               type="text"
@@ -106,16 +167,48 @@ const AppointmentForm = () => {
             </select>
             <select
               value={`${doctorFirstName} ${doctorLastName}`}
-              onChange={(event) => { const [firstName, lastName] = event.target.value.split("");
+              onChange={(event) => {
+                const [firstName, lastName] = event.target.value.split("");
                 setDoctorFirstName(firstName);
                 setDoctorLastName(lastName);
               }}
               disabled={!department}
             >
-              <option >Select Doctor</option>
-              {doctors}
+              <option value="">Select Doctor</option>
+              {doctors
+                .filter((doctor) => doctor.doctorDepartment === departmen)
+                .map((doctor, index) => (
+                  <option
+                    value={`${doctor.firstName} ${doctor.lastName}`}
+                    key={index}
+                  >
+                    {doctor.firstName} {doctor.lastName}
+                  </option>
+                ))}
             </select>
           </div>
+          <textarea
+            placeholder="Adresse"
+            rows="10"
+            value={adress}
+            onChange={(event) => setAdress(event.target.value)}
+          />
+          <div
+            style={{
+              gap: "10px",
+              justifyContent: "flex-end",
+              flexDirection: "row",
+            }}
+          >
+            <p style={{ marginBottom: 0 }}>Have you visited before?</p>
+            <input
+              type="checkbox"
+              value={hasVisited}
+              onChange={(event) => setVisited(event.target.checked)}
+              style={{ width: "25px", flex: "none" }}
+            />
+          </div>
+          <button style={{ margin: "0 auto" }}>GET APPOINTMENT</button>
         </form>
       </div>
     </>
